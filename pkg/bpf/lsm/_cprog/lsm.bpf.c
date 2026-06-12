1eebceaefd0fc8f823edcc406f7582c06fa42bc3
aerosouund <aerosound161@gmail.com>
2026-06-12 13:37:51 +0000

    ai
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- pkg/bpf/lsm/_cprog/lsm.bpf.c
+++ pkg/bpf/lsm/_cprog/lsm.bpf.c
@@ -30,29 +30,35 @@ struct {
 
 static __always_inline int handle_open(__u64 *args) {
     __u64 arg0 = args[0];
-    struct file *f = (struct file *)arg0; // cast arg0 to a file
+    struct file *f = (struct file *)arg0;
     char buf[MAX_PATH_LEN] = {};
     bpf_d_path(&f->f_path, buf, sizeof(buf));
 
     __u8 *val = bpf_map_lookup_elem(&banned, &buf);
     if (val) {
+        bpf_printk("lsm: denying open: path=%s", buf);
         return -EPERM;
     }
+    bpf_printk("lsm: allowing open: path=%s", buf);
     return 0;
 }
 
 static __always_inline int handle_exec(__u64 *args) {
     __u64 arg0 = args[0];
-    struct linux_binprm *bprm = (struct linux_binprm *)arg0; // cast arg0 to a binprm
-    char buf[MAX_PATH_LEN] = {};  // zero initialized, padding is automatic
+    struct linux_binprm *bprm = (struct linux_binprm *)arg0;
+    char buf[MAX_PATH_LEN] = {};
     const char *fname = BPF_CORE_READ(bprm, filename);
     int len = bpf_probe_read_kernel_str(buf, sizeof(buf), fname);
-    if (len <= 0)
+    if (len <= 0) {
+        bpf_printk("lsm: exec: failed to read filename (len=%d)", len);
         return 0;
+    }
     __u8 *val = bpf_map_lookup_elem(&banned, &buf);
-    if (val)
+    if (val) {
+        bpf_printk("lsm: denying exec: path=%s", buf);
         return -EPERM;
-
+    }
+    bpf_printk("lsm: allowing exec: path=%s", buf);
     return 0;
 }
 
@@ -62,17 +68,19 @@ int generic_lsm_handler(struct bpf_raw_tracepoint_args *ctx)
     __u64 cgid = bpf_get_current_cgroup_id();
     __u32 key = 0;
     __u64 *argtype = bpf_map_lookup_elem(&argtypes, &key);
-    if (!argtype)
+    if (!argtype) {
+        bpf_printk("lsm: no argtype configured, skipping");
         return 0;
+    }
 
-    __u64 *args = (__u64*)ctx; // cast context as a pointer to a u64 (array) 
-    // how does tetragon in userspace interpret what the kernel passed ?
+    __u64 *args = (__u64*)ctx;
 
-    // the cgroup ID that triggered the hook is not targeted by this policy
     __u8 *val = bpf_map_lookup_elem(&cgids, &cgid);
     if (!val) {
         return 0;
     }
+
+    bpf_printk("lsm: handler triggered: cgid=%llu argtype=%llu", cgid, *argtype);
 
     switch (*argtype) {
         case 1: {
@@ -81,7 +89,10 @@ int generic_lsm_handler(struct bpf_raw_tracepoint_args *ctx)
         case 2: {
             return handle_exec(args);
         }
-    };    
+        default: {
+            bpf_printk("lsm: unknown argtype=%llu", *argtype);
+        }
+    };
 
     return 0;
 }
